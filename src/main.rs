ed1f8cb6bd22237d447a4e6775b10f3ef864e721
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2023-10-15 21:24:02 +0100

    add tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::protocol::Message;
+
+#[tokio::main]
+async fn main() -> Result<(), ()> {
+    let address = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:6969".to_string());
+    let socket = TcpListener::bind(&address).await;
+    let listener = socket.expect("Failed to bind");
+    println!("Listening on: {}", address);
+    while let Ok((stream, address)) = listener.accept().await {
+        tokio::spawn(handle_connection)
+    }
 }
